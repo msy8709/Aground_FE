48cbe4f942ce21c9581c726ab8ba71a862a63c58
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-02-09 14:43:10 +0900

    자체 로그인, 회원가입 구현

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo5 from "../../assets/logo5.png";
 import SignupModal from "../Signin/SignupModal";
+import client from "../../Clients";
 
 function Login(props){
     const [userid, setUserid] = useState('');
@@ -13,6 +14,8 @@ function Login(props){
     const [isOpen, setIsOpen] = useState(false);
     const [isid,setIsid] = useState('');
     const [isPassword,setIsPassword] = useState('');
+    const [isLogin,setIsLogin] = useState('')
+    const navigate = useNavigate();
     const ModalOpen=()=>{
         setIsOpen(true);
     }
@@ -22,28 +25,42 @@ function Login(props){
     const handleIdChange = (e) =>{
         setUserid(e.target.value);
         setIsid(e.target.value.toLowerCase().match(/([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/))
-        console.log('id:', userid);
     }
     const handlePwChange = (e) => {
         setUserpw(e.target.value);
-        console.log('pw:',userpw);
         setIsPassword(e.target.value.toLowerCase().match(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/))
     }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('id:', userid);
-        
+
+    const onKakaoClick = (e) => {
+        window.location.replace("http://43.202.89.97:59988/api/login/kakao");
     }
-   
+    const onAgrooundClick = (e) => {
+        e.preventDefault();
+        const loginData = {
+            'user_id':userid,
+            'user_pw': userpw
+        }
+        client.post('/api/login/login/', loginData)
+        .then(function(response){
+                sessionStorage.setItem('nickname', response.data.user_nickname);
+                console.log(response.data.user_nickname);
+                navigate('/ALMainPage');
+        })
+        .catch(function(error){
+            setIsLogin(false);
+            alert(error.response.data.error)
+            console.log(error);
+        })
+    }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={onAgrooundClick}>
             {isOpen &&(<div className={styles.compbox}><SignupModal ModalClose = {ModalClose}/></div>)}
             <div className={styles.login}>
                 <div className={styles.logo}>AGROUNDS</div>
                 <div className={styles.idbox}><input onChange={handleIdChange} placeholder="아이디"className={styles.id} type="text"></input></div>
                 <div className={styles.pwbox}><input onChange={handlePwChange} placeholder="비밀번호"className={styles.pw} type="text"></input></div>
-                {isid && isPassword ?<Button onClick={handleSubmit} type="submit"color="#FFFFFF"backcolor="#055540" text="로그인" logoimg={logo5}/>:<Button type="button"color="white"backcolor="#dadada" text="로그인" logoimg={logo5}/>}
+                {isid && isPassword ?<Button onClick={onAgrooundClick} type="submit"color="#FFFFFF"backcolor="#055540" text="로그인" logoimg={logo5}/>:<Button type="button"color="white"backcolor="#dadada" text="로그인" logoimg={logo5}/>}
                 
                 <div className={styles.find}>
                     <div className={styles.signin} onClick={ModalOpen}>회원가입</div>
@@ -52,7 +69,7 @@ function Login(props){
                     <div className={styles.fline}></div>
                     <div className={styles.fpw}>비밀번호 찾기</div>
                 </div>
-                <Button logoimg={Kakao} color="black"backcolor="#FEE502" text="카카오톡으로 로그인"/>
+                <Button onClick={onKakaoClick}logoimg={Kakao} color="black"backcolor="#FEE502" text="카카오톡으로 로그인"/>
                 <div className={styles.ggback}>
                     <Button logoimg={Google} color="black"backcolor="#FFFFFF" text="구글로 로그인"/></div>
             </div>
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -12,32 +12,51 @@ function Signin({color,text,imgsrc}){
     const [birth,setBirth] = useState('');
     const [gender, setGender] = useState('');
     const [marketingAgree, setMarketingAgree] = useState('');
-    const [isNickname,setIsNickname] = useState(true);
-    const [isName,setIsName] = useState(true);
-    const [isBirth,setIsBirth] = useState(true);
+    const [termsAgree,setTermsAgree] = useState('');
+    const [privacyAgree,setPrivacyAgree] = useState('');
+    const [allAgree,setAllAgree] = useState('');
+    const [isNickname,setIsNickname] = useState('');
+    const [isName,setIsName] = useState('');
+    const [isBirth,setIsBirth] = useState('');
+    const [isDup,setIsDup] = useState('');
 
-    const isAllValid = isNickname && isName && isBirth;
+    
+    let isAgree = privacyAgree && termsAgree;
+    let isAllValid = isDup && isName && isBirth && isAgree;
+
+    useEffect(() => {
+        setAllAgree(privacyAgree && termsAgree && marketingAgree)
+    },[privacyAgree ,termsAgree, marketingAgree])
+
     const saveNickname = event => {
         setNickname(event.target.value);
-        setIsNickname(event.target.value.length > 2);
+        setIsDup('');
+        const IsValidNickname = /^[a-zA-Z가-힣0-9!@#$%^&*()-_=+{};:,<.>]{3,10}$/.test(event.target.value)
+        setIsNickname(IsValidNickname ? "1" : "0")
+        if(IsValidNickname === ""){
+            setIsNickname("2")
+        }
     }
     
     const saveName = event => {
         setName(event.target.value);
-        setIsName(/^[가-힣a-zA-Z]{2,20}$/.test(name))
-    }
+        const IsValidName = /^[가-힣a-zA-Z]{2,20}$/.test(event.target.value)
+        setIsName(IsValidName? "1" : "0");
+        if (event.target.value === "") {
+            setIsName("2");
+    }}
+    
     const saveBirth = event => {
         setBirth(event.target.value);
-        setIsBirth(/^\d{8}$/.test(birth))
-    }
+        const IsValidBirth = /^\d{8}$/.test(event.target.value)
+        setIsBirth(IsValidBirth? "1" : "0");
+        if (event.target.value === "") {
+            setIsBirth("2");
+    }}
     const saveGender = event => {
         setGender(event.target.value);
-        console.log(gender);
     }
-    const saveAgree = event => {
-        setMarketingAgree(event.target.value);
-        console.log(marketingAgree);
-    }
+    
     const onSubmitHandler = async event => {
         event.preventDefault();
 
@@ -57,18 +76,22 @@ function Signin({color,text,imgsrc}){
         .catch(function(error){
             console.log(error);
         })
-        const isdup = client.get('/api/login/signup/')
-            
-        
     }
-    
-        
-    
-    
+    const onDupCheckHandler = async event => {
+        event.preventDefault();
+        client.get('/api/login/nickname/?nickname=' + nickname)
+        .then(function(response){
+            setIsDup(response.data.isAvailable)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
   
 
     const sameBStyle = {
         border: `.2vh solid ${color}`,
+        
     }
     const SigninBStyle = {
         backgroundColor: color,
@@ -86,20 +109,32 @@ function Signin({color,text,imgsrc}){
                 <div className={styles.block2}>
                     <div className={styles.name}>닉네임</div>
                     <input onChange={saveNickname}className={styles.input}type="text" placeholder="3~8자리 입력"></input>
-                    {isNickname ? <div className={styles.checkdup2} >중복확인</div> : <div className={styles.checkdup} >중복확인</div>}
+                    {isNickname === "1" ? (
+                                <div className={styles.checkdup2} style={SigninBStyle}onClick={onDupCheckHandler}>중복확인</div>
+                            ) : (<div className={styles.checkdup} style={sameBStyle} >중복확인</div>)}
                 </div>
-                {isNickname ?(<p style={{color:"red"}}className={styles.errortext}>중복검사를 해주세요.</p>): (<p style={{color:"red"}}className={styles.errortext}>닉네임은 3글자 이상이어야 합니다.</p>)}
+                {isNickname === "0" ? (
+                            <p className={styles.errortext} style={{ color: "red" }}>3~8자리로 설정해주세요.</p>
+                        ): isNickname === "1" && isDup ?(
+                            <p className={styles.errortext} style={{ color: "rgb(60,100,233)" }}>사용가능한 닉네임입니다.</p>
+                        ): isNickname === "1" && isDup === false ?(
+                            <p className={styles.errortext} style={{ color: "red" }}>중복된 닉네임입니다.</p>
+                        ): isNickname === "1" ?(
+                            <p className={styles.errortext} style={{ color: "red" }}>중복검사를 실시해주세요.</p>
+                        ): (
+                            <p className={styles.errortext} style={{ color: "white" }}></p>
+                        )}
             </div>
             
             <TextInput isValid={isName} errortext="이름을 입력해주세요." correcttext=""onChange={saveName}text="이름" type="text" placeholder="실명 입력"/>
-                    <TextInput isValid={isBirth}errortext="YYYYMMDD 형식으로 입력해주세요." correcttext="생년월일이 입력되었습니다."onChange={saveBirth}text="생년월일" type="text" placeholder="8자리 (YYYYMMDD)" maxLength={8} pattern="\d*" />
-                    
-                    <div >
-                        <SelectGender setGender={setGender} onChange={saveGender} color="#055540"/>
-                    </div>
-                    
-                    <AgreeCheckbox setMarketingAgree={setMarketingAgree}onChange={saveAgree}color="#055540"/>
-                    {isAllValid ? <button style={SigninBStyle} type="submit"className={styles.submit}>가입하기</button> : <button type="button"className={styles.submit2}>가입하기</button>}
+            <TextInput isValid={isBirth}errortext="YYYYMMDD 형식으로 입력해주세요." correcttext="생년월일이 입력되었습니다."onChange={saveBirth}text="생년월일" type="text" placeholder="8자리 (YYYYMMDD)" maxLength={8} pattern="\d*" />
+            
+            <div >
+                <SelectGender setGender={setGender} onChange={saveGender} color={color}/>
+            </div>
+        
+            <AgreeCheckbox setTermsAgree={setTermsAgree}privacyAgree={privacyAgree}termsAgree={termsAgree}marketingAgree={marketingAgree}allAgree={allAgree}setAllAgree={setAllAgree}setMarketingAgree={setMarketingAgree}setPrivacyAgree={setPrivacyAgree} color={color}/>
+            {isAllValid ? <button style={SigninBStyle} type="submit"className={styles.submit}>가입하기</button> : <button type="button"className={styles.submit2}>가입하기</button>}
         </div>
       </form>
     )
